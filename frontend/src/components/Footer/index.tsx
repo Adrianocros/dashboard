9bab619d690c0f2d157973d7b87e3044a2e2d5b6
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2021-05-03 17:59:01 -0300

    Correção do nome

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/adrianocros" target="_blank" rel="noreferrer">Adrinao B. Crosoletto</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/adrianocros" target="_blank" rel="noreferrer">Adriano B. Crosoletto</a></p>
                 <p className="text-light"><small><strong>Spring with React</strong><br />
             Sistema de controle de vendas: <a href="--" target="_blank" rel="noreferrer">@</a></small></p> 
             </div>
