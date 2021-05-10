584263d8e3d6e867d2ec010ded9e606342c637c6
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2021-05-10 13:22:57 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,11 +1,34 @@
-export type SaleSum =  {
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    number: number;
+    size?: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
+export type SaleSum = {
     name: string;
     sum: number;
 }
 
-export type SaleSucess =  {
-    "sellerName": string;
-    "visited": number;
-    "deals": number;
+export type SaleSucess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
 
